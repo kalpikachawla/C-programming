3ac259436d79e21333e41427dfc3cc9ae8f1ed3b
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-17 16:52:56 +0530

    clear spaces

---
+++ prac2.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the value of x :");
+    scanf("%d",&x);
+    int s;
+    s=(x)*(x);
+    printf("The square of number is :%d",s);
+   
+    return 0;
+}
