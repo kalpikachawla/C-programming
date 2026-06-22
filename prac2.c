f0f26d9807bb98c7a7527e7a642fb26232b47485
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-22 22:09:49 +0530

    printed the square of the number

--- prac2.c
+++ prac2.c
@@ -1,11 +1,19 @@
 # include <stdio.h>
 int main(){
-    int x,y;
+    float x,y,z,a,b,p;
     printf("Enter the value of x :");
-    scanf("%d",&x);
-    int s;
-    s=(x)*(x);
-    printf("The square of number is :%d",s);
-   
-    return 0;
+    scanf("%f",&x);
+printf("Enter the value of y :");
+    scanf("%f",&y);
+    printf("Enter the value of z :");
+    scanf("%f",&z);
+    printf("Enter the value of a :");
+    scanf("%f",&a);
+    printf("Enter the value of b :");
+    scanf("%f",&b);
+    p=(x+y+z+a+b)/5*(100);
+    printf("The percentage is %f",p);
+    
+        return 0;
 }
+   
