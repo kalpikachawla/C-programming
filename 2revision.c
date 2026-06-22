e76c838e5ea37c454f2bca82d3d046950345e885
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-22 22:05:09 +0530

    printed star square

--- 2revision.c
+++ 2revision.c
@@ -6,8 +6,8 @@ int main(){
     printf("Enter the number of columns :");
     scanf("%d",&x);
     for(int i=1;i<=n; i++){
-        for(int j=1;j<=i;j++){
-            printf("%d ",j);
+        for(int j=1;j<=n;j++){
+            printf("* ");
         }
         printf("\n");
     }
