fff65514a244d336922968a606c0446b080fe419
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-22 22:10:28 +0530

    printed star square

--- squaresolid.c
+++ squaresolid.c
@@ -1,13 +1,10 @@
 # include <stdio.h>
 int main(){
     int n;
-    int m;
     printf("Enter number of rows :");
     scanf("%d",&n);
-    printf("Enter number of columns :");
-    scanf("%d",&m);
     for(int i=1; i<=n; i++){
-        for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
             printf("*");
         }
         printf("\n");
