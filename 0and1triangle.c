cc5333914d4fda0b6e89891b0db359a9145b6976
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-22 22:03:27 +0530

    printed 0 and 1 triangle

--- 0and1triangle.c
+++ 0and1triangle.c
@@ -4,16 +4,4 @@ int main(){
     printf("Enter number of rows :");
     scanf("%d",&n);
 int a;
-    for(int i=1;i<=n;i++){
-        if(i%2!=0) a=1; // start 
-        else a=0;
-        for(int j=1;j<=i;j++){
-            if(a==0) a=1;// switch
-            else a=0;
-            printf("%d ",a);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
+   
