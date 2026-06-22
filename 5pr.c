5575c8c1e8174b72aba9c550c1e520c2d519263c
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-22 22:07:03 +0530

    if-else to tell positive , negative and zero

--- 5pr.c
+++ 5pr.c
@@ -1,14 +1,18 @@
 # include <stdio.h>
 int main(){
     int x;
+
     printf("Enter x :");
+
     scanf("%d",&x);
+
    if(x>0){
     printf("%d is positive number",x);
    }
    else if(x<0){
     printf("%d is negative number",x);
     }
+    
     else {
         printf("%d is neither positive nor negative",x);
     }
