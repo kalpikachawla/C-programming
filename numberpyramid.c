3faf8139e36a42ff8af89dbda8265d61c685a729
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-24 12:57:16 +0530

    printed number pyramid

--- numberpyramid.c
+++ numberpyramid.c
@@ -1,19 +1,24 @@
 # include <stdio.h>
 int main(){
-     int n;
-     printf("Enter number of lines :");
-     scanf("%d",&n);
-     int nst=1;
-     int nsp=3;// for spaces
-     for(int i=1; i<=n; i++){
-        for(int k=1; k<=nsp; k++)
-        printf(" ");
-        nsp=nsp-1;
-        for(int j=1; j<=nst; j++)
-        printf("%d",j);
-        nst=nst+2;
-        printf("\n");
-        
-     }
-    return 0;
+   int n;
+   printf("Enter number of lines :");
+   scanf("%d",&n);
+   int nsp=n-1;
+   for(int i=1; i<=n; i++){
+      int a=i-1;
+      for(int q=1; q<=nsp; q++){
+         printf(" ");
+      }
+      nsp--;
+      for(int j=1; j<=i; j++){
+         printf("%d",j);
+      }
+      for(int k=1; k<=i-1; k++){
+         printf("%d",a);
+         a--;
+      }
+      printf("\n");
+   }
+return 0;
 }
+   
