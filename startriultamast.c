a6eebf1d472fda6d8f2eebb68069a7d833c1e835
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-06-25 12:08:16 +0530

    printed inverted star triangle

---
+++ startriultamast.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of lines : ");
+    scanf("%d",&n);
+   int nsp=0;
+   int nst=n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=nsp; j++){
+            printf(" ");
+        }
+        nsp++;
+        for(int k=1; k<=nst; k++){
+            printf("*");
+        }
+        nst--;
+        printf("\n");
+    }
+    return 0;
+}
+    
