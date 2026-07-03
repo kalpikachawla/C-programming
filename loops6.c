ca53962cda71d336d5a067a436320b8d45e800f3
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-03 09:37:49 +0530

    factorial of number

---
+++ loops6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    int fact=1;
+    for(int i=2; i<=n; i++){
+        fact=fact*i;
+ }
+    printf("%d",fact);
+    return 0;
+}
