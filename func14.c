51e12e8e02ab90b40208750c958a43c6589c94d1
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-04 11:26:47 +0530

    printed power of a number using inbulit function

---
+++ func14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b;
+    printf("Enter base :");
+scanf("%d",&a);
+printf("Enter power :");
+scanf("%d",&b);
+int c = pow(a,b);
+printf("%d",c);
+return 0;
+} 
