c76ec521caf22d739a335e67439da628d85bc68f
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-05 12:33:00 +0530

    printed name using a function

---
+++ func19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void  name(int a){
+    for(int i=1; i<=a; i++){
+        printf("KALPIKA");
+        printf("\n");
+    }
+    return;
+}
+int main(){
+    int n=10;
+    name(n);
+
+    return 0;
+}
