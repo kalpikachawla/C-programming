cf928a0499d031a4b1814693565744b3df55c7cf
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-10 23:22:44 +0530

    printed reverse of number using functions

---
+++ func25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void reverse(int a){
+    int r=0;
+    while(a>0){
+        r=r*10;
+        r=r+(a%10);
+        a=a/10;
+    }
+    printf("The reverse of number is %d",r);
+    return;
+   
+}
+int main(){
+    int n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    reverse(n);
+    return 0;
+}
