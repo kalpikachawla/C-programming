6ebc2b11592152407fd3d48d24a1dee31985543e
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-14 11:50:15 +0530

    printed increasing and decreasing numbers combinely using recursion

---
+++ decincre.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void incdec(int n){
+    if(n==0) return;
+     printf("%d\n",n);
+     incdec(n-1);
+      printf("%d\n",n);
+      return;
+}
+int main(){
+    int n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    incdec(n);
+
+    return 0;
+}
