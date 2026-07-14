0ddd297844503f57dce39c94d944afd2c8eae238
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-14 12:15:37 +0530

    printed sum of numbers using return type

---
+++ sumreturntype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int sum(int n){
+    if(n==1) return 1; // base case
+       return n+sum(n-1);
+}
+    int main(){
+    int n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    int fact=sum(n);
+    printf("%d",fact);
+    return 0;
+}
