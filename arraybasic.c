ea914993b3182c3d109a4900b1347001507c30d1
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-17 11:41:26 +0530

    Basics of arrays with all three datatypes

---
+++ arraybasic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={2,4,6,8,9};// declaration and insialisation
+    arr[4]=12;
+    printf("%d ",arr[4]);// printing by using index
+    float brr[2]={2.9,2.4};
+    printf("%f ",brr[1]);
+   char chrr[2]={'r','i'};
+   printf("%c ",chrr[0]);
+    return 0;
+}
