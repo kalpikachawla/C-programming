07d7512ab387a02b3688000107f8908ae49f90c8
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-17 12:45:53 +0530

    printed address in arrays

---
+++ address.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={1,2,2,4,7};
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[0]);
+
+    return 0;
+}
