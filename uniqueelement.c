723893519b7d5ad9e020445f9b4d452c47b571de
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-21 12:25:39 +0530

    printed unique element

---
+++ uniqueelement.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int arr[5] = {1,2,4,2,1};
+    for(int i=0; i<=4; i++){
+        bool flag = false;
+        for(int j=i+1; j<=4; j++){
+            if(arr[i]==arr[j]){
+               flag = true;
+            }
+             }
+             if(flag==false){
+                printf("%d",arr[i]);
+                break;
+            }
+       
+    }
+     
+    return 0;
+}
