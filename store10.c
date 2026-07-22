46f0bad1b35eefd745b050abedc72d8a7e5ccf12
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-22 12:00:22 +0530

    stored 10 into 5*5 matrix

---
+++ store10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter rows :");
+    scanf("%d",&r);
+    printf("Enter columns :");
+    scanf("%d",&c);
+    int arr[r][c];
+    printf("Enter elements : \n");
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            scanf("%d",&arr[i][j]);
+        }
+     }
+     printf("\n");
+     for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+     }
+
+    return 0;
+}
