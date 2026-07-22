53c4b159e7fba625d0a98f12c810f969c23164b7
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-22 11:52:04 +0530

    printed student roll no and marks (homework) - 2D arrays

---
+++ studentmarks.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter rows :");
+    scanf("%d",&r);
+    printf("Enter columns :");
+    scanf("%d",&c);
+   int arr[r][c];
+   printf("Enter marks :\n");
+   for(int i=0; i<r; i++){
+    for(int j=0; j<c; j++){
+        scanf("%d",&arr[i][j]);
+    }
+   }
+   printf("Student details :\n");
+   for(int i=0; i<r; i++){
+    for(int j=0; j<c; j++){
+        printf("%d ",arr[i][j]);
+    }
+     printf("\n");
+
+   }
+  
+   
+
+    return 0;
+}
