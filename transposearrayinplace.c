5cfd58ab57e439e4169648237644c913143357e4
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-23 10:19:34 +0530

    transposed array in place

---
+++ transposearrayinplace.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter rows/columns :");
+    scanf("%d",&n);
+    int arr[n][n];
+    printf("Enter all the elements :\n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+        scanf("%d",&arr[i][j]);
+    }
+    printf("\n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+     for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+     printf("%d ",arr[i][j]);
+            }
+            printf("\n");
+          }
+
+    return 0;
+}
