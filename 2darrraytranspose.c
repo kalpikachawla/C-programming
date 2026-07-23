e0335744f1b4ecdf20f079a6c1165c255cfb1fe1
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-23 09:57:44 +0530

    transposed array and stored it

---
+++ 2darrraytranspose.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter rows :");
+    scanf("%d",&r);
+    printf("Enter columns :");
+    scanf("%d",&c);
+    int arr[r][c];
+    printf("Enter all the elements :\n");
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++)
+        scanf("%d",&arr[i][j]);
+    }
+    printf("\n");
+    int brr[c][r];
+    for(int i=0; i<c; i++){
+        for(int j=0; j<r; j++){
+           // printf("%d",arr[j][i]);
+           brr[i][j] = arr[j][i];
+        }
+         printf("\n");
+          }
+          for(int i=0; i<c; i++){
+            for(int j=0; j<r; j++){
+                printf("%d ",brr[i][j]);
+            }
+            printf("\n");
+          }
+
+    return 0;
+}
