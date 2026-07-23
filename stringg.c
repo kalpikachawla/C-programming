77f7a49f668439c5de490beb77112f0842087d96
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-23 16:30:18 +0530

    taken input of characters and printed it using puts() and gets()

---
+++ stringg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<strings.h>
+int main(){
+    // char str[] = "College Wallah";
+    // puts(str);
+   char  str[5];
+    gets(str);
+    printf("Your input is : %s",str);
+   return 0;
+}
