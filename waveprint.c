d48b195822fe764cf6d55278ece70dd61513f48f
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-23 13:10:34 +0530

    wave printing of array

---
+++ waveprint.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter rows :");
+    scanf("%d",&r);
+    printf("Enter columns :");
+    scanf("%d",&c);
+    int arr[r][c];
+    printf("Enter elements of array :");
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\n");
+    for(int i=0; i<r; i++){
+        if(i%2==0){
+            for(int j=0; j<c; j++){
+                printf("%d ",arr[i][j]);
+            }
+            printf("\n");
+        }
+        else{
+                for(int j=c-1; j>=0; j--){
+                    printf("%d ",arr[i][j]);
+                }
+                printf("\n");
+            }
+    }
+  return 0;
+}
